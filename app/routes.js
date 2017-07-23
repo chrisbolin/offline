1ee7bbc6c6a65ececc82e7111d9a7c37495431ba
Chris Bolin <bolin.chris@gmail.com>
2017-07-23 13:38:51 -0400

    catch-all forwarding route

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,7 @@
 // They are all wrapped in the App component, which should contain the navbar etc
 // See http://blog.mxstbr.com/2016/01/react-apps-with-pages for more information
 // about the code splitting business
-import { getAsyncInjectors } from 'utils/asyncInjectors';
+import { push } from 'react-router-redux';
 
 const errorLoading = (err) => {
   console.error('Dynamic page loading failed', err); // eslint-disable-line no-console
@@ -13,9 +13,6 @@ const loadModule = (cb) => (componentModule) => {
 };
 
 export default function createRoutes(store) {
-  // Create reusable async injectors using getAsyncInjectors factory
-  const { injectReducer, injectSagas } = getAsyncInjectors(store); // eslint-disable-line no-unused-vars
-
   return [
     {
       path: '/',
@@ -36,10 +33,8 @@ export default function createRoutes(store) {
     }, {
       path: '*',
       name: 'notfound',
-      getComponent(nextState, cb) {
-        import('containers/NotFoundPage')
-          .then(loadModule(cb))
-          .catch(errorLoading);
+      getComponent() {
+        store.dispatch(push('/'));
       },
     },
   ];
