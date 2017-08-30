e4deab6889e44d7807bb9676f5c9914ef23c494e
Chris Bolin <bolin.chris@gmail.com>
2017-08-30 04:37:26 -0600

    add translations

--- src/App.js
+++ src/App.js
@@ -1,181 +1,17 @@
 import React from 'react';
 import { Offline, Online } from 'react-detect-offline';
-import styled, { injectGlobal } from 'styled-components';
 
-const MOBILE = /Mobile|Android/.test(navigator.userAgent);
-
-const colors = {
-  black: '#181e1e',
-  white: '#f6f6f6',
-  blueGrey: '#a0a9bb',
-  funBlue: '#7f8ed1',
-  selectionGreen: '#47ff90',
-};
-
-/* eslint no-unused-expressions: 0 */
-injectGlobal`
-  html,
-  body {
-    background-color: ${colors.black};
-    margin: 0;
-    padding: 0;
-  }
-
-  ::selection {
-    background: ${colors.selectionGreen};
-  }
-
-  @keyframes fadeIn {
-    0% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 100;
-    }
-  }
-
-  @keyframes fadeInAndRise {
-    0% {
-      color: white;
-      transform: translateY(2%);
-    }
-    100% {
-      color: black;
-      transform: translateY(0);
-    }
-  }
-`;
-
-const Page = styled.div`
-  min-height: 100vh;
-  padding: 2px;
-  line-height: 1.5;
-
-  /* medium / wordpress approach https://css-tricks.com/snippets/css/system-font-stack/ */
-`;
-
-const Content = `
-  margin: auto;
-  min-width: 310px;
-  width: 80%;
-  max-width: 600px;
-`;
-
-const TextBody = styled.div`
-  ${Content}
-  min-width: 300px;
-  font-family: "Apple Garamond", "Baskerville", "Times New Roman", "Droid Serif", "Times","Source Serif Pro", serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-  padding: 20px 0 50px;
-  font-size: 22px;
-  animation: 700ms ease-out 0s 1 fadeInAndRise;
-`;
-
-const BlocklyHeader = styled.div`
-  ${Content}
-  font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif,"Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-  font-size: calc(2.5em + 4.2vmin);
-  line-height: 130%;
-  color: ${colors.white};
-  font-weight: 700;
-  padding: 50px 0 10px;
-  animation: 300ms ease-out 0s 1 fadeIn;
-  h1 {
-    display: inline;
-    background-color: ${(p) => p.highlighterColor};
-    font-size: 100%;
-  }
-  h2 {
-    display: inline;
-    background-color: ${(p) => p.highlighterColor};
-    font-size: 25px;
-    margin: 0;
-    padding: 0;
-    line-height: 170%;
-  }
-`;
-
-const OnlineContent = () => (
-  <Page style={{ backgroundColor: colors.blueGrey }}>
-    <BlocklyHeader highlighterColor={colors.black}>
-      <h1>
-        You must go offline to view this page.
-      </h1>
-      <br />
-      <h2>
-        { MOBILE ? 'Enable Airplane Mode.' : 'Disconnect to continue'}
-      </h2>
-    </BlocklyHeader>
-  </Page>
-);
-
-const OfflineContent = () => (
-  <Page style={{ backgroundColor: colors.white }}>
-    <BlocklyHeader highlighterColor={colors.funBlue}>
-      <h1>
-        Offline Only
-      </h1>
-    </BlocklyHeader>
-    <TextBody>
-      <p><i>2017. 2-minute read.</i></p>
-      <p>
-        Do you want to be productive?
-        Just go offline, because to maintain a constant connection to the internet is to maintain a constant connection to interruptions, both external and internal.
-      </p>
-
-      <p>
-        The external interruptions are legion and well documented:
-				you have a new message on Gmail, Slack, Twitter, Facebook, Instagram, Snapchat, LinkedIn. Friends, family, coworkers, and spammers: each have direct access to your precious attention.
-      </p>
-
-      <p>
-        But it’s the internal distractions that are truly pernicious.
-        You can mute Twitter notifications and log off from Slack, but how do you stop your own mind from derailing you?
-			</p>
-
-			<p>
-        I have spent hours caught in webs of my own curiosity.
-        Most dangerous is the split-second whim:
-        “I wonder what the second most commonly spoken language is?”
-        Those 500 milliseconds could change your day, because it's never just one Google search, never just one Wikipedia article.
-        Disconnecting from the internet short-circuits those whims, allowing you to move on unencumbered.
-      </p>
-
-      <p>
-				(It’s Spanish, by the way.)
-      </p>
-
-      <p>
-        This page itself is an experiment in that vein: What if certain content required us to disconnect?
-        What if readers had access to that glorious focus that makes devouring a novel for hours at a time so satisfying?
-        What if creators could pair that with the power of modern devices?
-        Our phones and laptops are amazing platforms for inventive content—if only we could harness our own attention.
-      </p>
-
-      <p>
-        Offline-only content would also force creators to think differently.
-        Look at this page: there is not a single link to distract readers.
-        How many good articles have you left half-read because you chased a shiny underlined link?
-        When you are offline, right here is the only place you can be.
-      </p>
-
-      <p>
-        I can already hear the groans: “But I have to be online for my job.” I don’t care. Make time.
-        I bet the thing that makes you valuable is not your ability to Google something but your ability to synthesize information.
-				Do your research online; create offline.
-      </p>
-
-      <p>
-        Now back to your regularly scheduled internet. Just remember to give yourself an occasional gift of disconnection.
-      </p>
-
-      <p>
-        <span role="img" aria-label="love">❤️</span> Chris Bolin <i>(@bolinchris when you reconnect)</i>
-      </p>
-    </TextBody>
-  </Page>
-);
+import { OnlineContent, OfflineContent, LanguageSwitcher } from './components';
+import { languages } from './texts';
 
 export default class OfflinePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+	constructor() {
+		super();
+		const language = navigator.language.split('-')[0].toLowerCase();
+		this.state = {
+			language: languages[language] ? language : 'en',
+		};
+	}
 	componentDidMount() {
 		console.log('But how does it work?');
 		console.log(`Check out https://github.com/chrisbolin/react-detect-offline (if you're online!)`);
@@ -187,11 +23,16 @@ export default class OfflinePage extends React.PureComponent { // eslint-disable
 			console.log(`Still didn't work? Damn. I'm just really sorry.`);
 		};
 	}
+	onLanguageChange = event => this.setState({ language: event.target.value})
   render() {
     return (
       <div>
-        <Offline><OfflineContent /></Offline>
-        <Online><OnlineContent /></Online>
+				<LanguageSwitcher
+					language={this.state.language}
+					onChange={this.onLanguageChange}
+				/>
+        <Offline><OfflineContent language={this.state.language} /></Offline>
+        <Online><OnlineContent language={this.state.language} /></Online>
       </div>
     );
   }
---
+++ src/texts.js
@@ -0,0 +1,216 @@
+import React from 'react';
+
+const MOBILE = /Mobile|Android/.test(navigator.userAgent);
+
+export const languages = {
+	en: {
+		onlineElements: () => [
+			<h1>
+				You must go offline to view this page.
+			</h1>,
+			<br />,
+			<h2>
+				{ MOBILE ? 'Enable Airplane Mode.' : 'Disconnect to continue'}
+			</h2>
+		],
+		title: () => 'Offline Only',
+		bodyParagraphs: () => [
+			<p><i>2017. 2-minute read.</i></p>,
+			<p>
+				Do you want to be productive?
+				Just go offline, because to maintain a constant connection to the internet is to maintain a constant connection to interruptions, both external and internal.
+			</p>,
+
+			<p>
+				The external interruptions are legion and well documented:
+				you have a new message on Gmail, Slack, Twitter, Facebook, Instagram, Snapchat, LinkedIn. Friends, family, coworkers, and spammers: each have direct access to your precious attention.
+			</p>,
+
+			<p>
+				But it’s the internal distractions that are truly pernicious.
+				You can mute Twitter notifications and log off from Slack, but how do you stop your own mind from derailing you?
+			</p>,
+
+			<p>
+				I have spent hours caught in webs of my own curiosity.
+				Most dangerous is the split-second whim:
+				“I wonder what the second most commonly spoken language is?”
+				Those 500 milliseconds could change your day, because it's never just one Google search, never just one Wikipedia article.
+				Disconnecting from the internet short-circuits those whims, allowing you to move on unencumbered.
+			</p>,
+
+			<p>
+				(It’s Spanish, by the way.)
+			</p>,
+
+			<p>
+				This page itself is an experiment in that vein: What if certain content required us to disconnect?
+				What if readers had access to that glorious focus that makes devouring a novel for hours at a time so satisfying?
+				What if creators could pair that with the power of modern devices?
+				Our phones and laptops are amazing platforms for inventive content—if only we could harness our own attention.
+			</p>,
+
+			<p>
+				Offline-only content would also force creators to think differently.
+				Look at this page: there is not a single link to distract readers.
+				How many good articles have you left half-read because you chased a shiny underlined link?
+				When you are offline, right here is the only place you can be.
+			</p>,
+
+			<p>
+				I can already hear the groans: “But I have to be online for my job.” I don’t care. Make time.
+				I bet the thing that makes you valuable is not your ability to Google something but your ability to synthesize information.
+				Do your research online; create offline.
+			</p>,
+
+			<p>
+				Now back to your regularly scheduled internet. Just remember to give yourself an occasional gift of disconnection.
+			</p>,
+
+			<p>
+				<span role="img" aria-label="love">❤️</span> Chris Bolin
+				<br/>
+				<i>(@bolinchris when you reconnect)</i>
+			</p>,
+		],
+	},
+	fr: {
+		onlineElements: () => [
+			<h1>
+				Vous devez être déconnecté pour consulter cette page.
+			</h1>,
+			<br />,
+			<h2>
+				{ MOBILE ? 'Activer le mode Avion.' : 'Déconnectez-vous pour continuer.'}
+			</h2>
+		],
+		title: () => 'Déconnecté uniquement',
+		bodyParagraphs: () => [
+			<p><i>2017. 2-minute read.</i></p>,
+			<p>
+				Do you want to be productive?
+				Just go offline, because to maintain a constant connection to the internet is to maintain a constant connection to interruptions, both external and internal.
+			</p>,
+
+			<p>
+				The external interruptions are legion and well documented:
+				you have a new message on Gmail, Slack, Twitter, Facebook, Instagram, Snapchat, LinkedIn. Friends, family, coworkers, and spammers: each have direct access to your precious attention.
+			</p>,
+
+			<p>
+				But it’s the internal distractions that are truly pernicious.
+				You can mute Twitter notifications and log off from Slack, but how do you stop your own mind from derailing you?
+			</p>,
+
+			<p>
+				I have spent hours caught in webs of my own curiosity.
+				Most dangerous is the split-second whim:
+				“I wonder what the second most commonly spoken language is?”
+				Those 500 milliseconds could change your day, because it's never just one Google search, never just one Wikipedia article.
+				Disconnecting from the internet short-circuits those whims, allowing you to move on unencumbered.
+			</p>,
+
+			<p>
+				(It’s Spanish, by the way.)
+			</p>,
+
+			<p>
+				This page itself is an experiment in that vein: What if certain content required us to disconnect?
+				What if readers had access to that glorious focus that makes devouring a novel for hours at a time so satisfying?
+				What if creators could pair that with the power of modern devices?
+				Our phones and laptops are amazing platforms for inventive content—if only we could harness our own attention.
+			</p>,
+
+			<p>
+				Offline-only content would also force creators to think differently.
+				Look at this page: there is not a single link to distract readers.
+				How many good articles have you left half-read because you chased a shiny underlined link?
+				When you are offline, right here is the only place you can be.
+			</p>,
+
+			<p>
+				I can already hear the groans: “But I have to be online for my job.” I don’t care. Make time.
+				I bet the thing that makes you valuable is not your ability to Google something but your ability to synthesize information.
+				Do your research online; create offline.
+			</p>,
+
+			<p>
+				Now back to your regularly scheduled internet. Just remember to give yourself an occasional gift of disconnection.
+			</p>,
+
+			<p>
+				<span role="img" aria-label="love">❤️</span> Chris Bolin
+				<br/>
+				<i>(@bolinchris quand vous serez reconnecté)</i>
+			</p>,
+			<p>
+				Traduit par Kévin Zanon.
+			</p>
+		],
+	},
+	it: {
+		onlineElements: () => [
+			<h1>
+				Devi disconnetterti per leggere questa pagina.
+			</h1>,
+			<br />,
+			<h2>
+				{ MOBILE ? 'Abilita la modalità aereo.' : 'Disconnettiti per continuare'}
+			</h2>
+		],
+		title: () => 'Esclusivamente Offline',
+		bodyParagraphs: () => [
+			<p><i>2017 - Tempo di Lettura: 2 minuti</i></p>,
+			<p>
+				Vorresti essere più produttivo? Allora passa all’offline, perché avere una connessione costante a internet significa avere un contatto costante con le interruzioni, sia esterne che interne.
+			</p>,
+			<p>
+				Le interruzioni esterne sono infinite e ben documentate: è quando ricevi un nuovo messaggio su Gmail, Slack, Twitter, Facebook, Instagram, Snapchat e LinkedIn. Amici, famigliari, colleghi e mittenti indesiderati: ognuno ha direttamente accesso alla tua preziosa attenzione.
+			</p>,
+			<p>
+				Sono però le interruzioni interne ad essere davvero pericolose. Puoi silenziare le notifiche di Twitter e scollegarti da Slack, è vero, ma come si fa a rimanere concentrati?
+			</p>,
+			<p>
+				Trascorrevo ore sui siti web di mio interesse. La minaccia più grande alla mia capacità di rimanere concentrato è il capriccio improvviso: “Mi domando quale sia la seconda lingua più parlata al mondo”.
+			</p>,
+			<p>
+				Quei 500 millisecondi potrebbero cambiare la tua giornata, perché non è mai solo una ricerca su Google o un articolo su Wikipedia.
+			</p>,
+			<p>
+				Disconnettersi ti permetterà di sabotare quelle voglie dell’ultimo secondo e potrai procedere liberamente. (Per tornare al quesito: la seconda lingua più parlata al mondo è lo spagnolo, comunque).
+			</p>,
+			<p>
+				In un certo senso, la pagina che stai leggendo è un esperimento: cosa succederebbe se per accedere ad alcuni contenuti fosse necessario disconnettersi?
+			</p>,
+			<p>
+				Cosa accadrebbe se i lettori potessero arrivare proprio al momento più importante di un racconto, in cui sono così concentrati da evitare di pensare ad altro e potessero davvero immergersi nella storia?
+			</p>,
+			<p>
+				Cosa accadrebbe se i creativi potessero combinare questa particolarità, sfruttando la potenza dei dispositivi tecnologici? I nostri cellulari e i nostri computer sono piattaforme straordinarie per creare contenuti ingegnosi, se solo potessimo controllare la nostra attenzione.
+			</p>,
+			<p>
+				Rendere disponibili i contenuti esclusivamente offline obbligherebbe i creativi a pensare diversamente. Guarda questa pagina: non c’è alcun link che possa distrarre il lettore. Quanti articoli interessanti avete lasciato a metà, perché un link sottolineato tutto scintillante ha catturato la vostra attenzione? Quando siete offline, è quello l’unico posto in cui vi trovate.
+			</p>,
+			<p>
+				Già le sento le lamentele: “Ma io ci devo essere online, è per lavoro”. Non mi importa. Trova il tempo. Scommetto che ciò che ti rende così speciale non è la tua capacità di cercare qualcosa su Google, bensì la capacità di sintesi. Fa’ ricerca online e crea offline.
+			</p>,
+			<p>
+				Ora torna pure alle tue attività su Internet. Ricorda slo di fare a te stesso un piccolo regalo ogni tanto: disconnettiti.
+			</p>,
+			<p>
+				<span role="img" aria-label="love">❤️</span> Chris Bolin
+				<br/>
+				<i>(@bolinchris quando ti riconnetti)</i>
+			</p>,
+			<p>
+				Tradotto da Emanuela Goldoni.
+			</p>
+
+		],
+	},
+};
+
+
+export const bodyParagraphs = (language) => languages[language].bodyParagraphs();
+export const onlineElements = (language) => languages[language].onlineElements();
+export const title = (language) => languages[language].title();
