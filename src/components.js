cba96043c2896f7d53410b0b98b286efa43a77b8
Frank Dietrich <bits_n_bytes@gmx.de>
2017-09-05 00:34:51 +0200

    add German to the LanguageSwitcher

--- src/components.js
+++ src/components.js
@@ -138,6 +138,7 @@ export const OfflineContent = ({ language }) => (
 export const LanguageSwitcher = ({language, onChange}) => (
 	<Select value={language} onChange={onChange} >
 		<option value="en">english</option>
+		<option value="de">deutsch</option>
 		<option value="fr">français</option>
 		<option value="it">italiano</option>
 	</Select>
